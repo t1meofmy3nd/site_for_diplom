b59e5e9c606b69ea8c5908c8898f81a714153857
IvanPetrov <ivan.petrov@gmail.com>
2025-06-27 10:24:49 +0400

    Перевод readme

--- header.php
+++ header.php
@@ -1,14 +1,14 @@
 <header>
-  <a href="index.html" class="logo">
+  <a href="index.php" class="logo">
     <img src="images/logo.png" alt="МедТех" />
   </a>
   <button class="menu-toggle" aria-label="Меню"><i class="fa-solid fa-bars"></i></button>
   <nav>
-    <a href="index.html">Главная</a>
-    <a href="catalog.html">Каталог</a>
-    <a href="services.html">Услуги</a>
-    <a href="about.html">О нас</a>
-    <a href="cart.html" class="cart-link"><i class="fa-solid fa-cart-shopping"></i><span class="cart-count">0</span></a>
+    <a href="index.php">Главная</a>
+    <a href="catalog.php">Каталог</a>
+    <a href="services.php">Услуги</a>
+    <a href="about.php">О нас</a>
+    <a href="cart.php" class="cart-link"><i class="fa-solid fa-cart-shopping"></i><span class="cart-count">0</span></a>
     <a href="profile.php" class="account-link" style="display:none;"><i class="fa-solid fa-user"></i> Личный кабинет</a>
     <a href="login.php" class="login-link"><i class="fa-solid fa-right-to-bracket"></i> Войти</a>
     <a href="register.php" class="register-link"><i class="fa-solid fa-user-plus"></i> Регистрация</a>
